dd89081f50c300aa8c43f7d673dd5aa294ab439a
marata13 <ioakeim13@hotmail.gr>
2022-06-09 15:04:52 +0300

    Genetic algorithm
    Implemented a genetic algorithm to solve the Traveling Salesman Problem

---
+++ GeneticAlgorithmTSP/Program.cs
@@ -0,0 +1,258 @@
+﻿
+
+Random random = new();
+
+Console.WriteLine("Enter the ammount of cities:");
+
+int numOfCities = Convert.ToInt32(Console.ReadLine());
+
+int maxCost = 10;
+
+int[,] adjacencyMatrix = CreateAdjacencyMatrix(numOfCities, maxCost, random);
+
+
+(float maxSuitability , int[] solution, int numberOfGenerations) = geneticAlgorithmTSP(adjacencyMatrix, 200,
+                    (float)1 / (numOfCities * (maxCost/2-1)), (float)0.8, (float)0.1, random);
+
+if (solution.Length != solution.Distinct().Count())
+{
+    Console.WriteLine("Contains duplicates");
+}
+else
+{
+    Console.WriteLine("the number of generations was :" + numberOfGenerations);
+    Console.WriteLine("the solutions costs is :" + (1 / maxSuitability));
+    Console.WriteLine("The solution is");
+    foreach (var item in solution)
+    {
+        Console.WriteLine(item);
+        Console.WriteLine((char)25);
+    }
+    Console.WriteLine(solution[0]);
+}
+
+
+static (float, int[], int) geneticAlgorithmTSP(int[,] adjacencyMatrix, int populationNumber, float suitabilityLimit,
+                float replacementPercent, float mutationPercent, Random random)
+{
+    float[] suitability = new float[populationNumber];
+    int numberOfGenerations=0;
+
+    List<int[]> population = InitializePopulation(populationNumber, adjacencyMatrix.GetLength(0), random);
+
+    //Calculating suitability for each chromosome
+    for (
+        int i = 0; i < population.Count; i++)
+    {
+        suitability[i] = Suitability(population[i], adjacencyMatrix);
+    }
+
+    //Main loop
+    while(numberOfGenerations < 1000 )
+    {
+        //Checking if a satisfactory solution has been found
+        if (suitability.Max() > suitabilityLimit)
+        {
+            break;
+        }
+
+
+        List<int[]> newPopulation = new();
+        float sum = suitability.Sum();
+
+        //Choosing the chromosomes that will not be replaced with roullete selection and placing them to the new population
+        for (int l = 0; l < (1-replacementPercent)*population.Count; l++)
+        {
+            for (float i = 0, pick = random.NextSingle() * sum; (int)i < population.Count; i += 1, pick -= suitability[(int)i-1])
+            {
+                if (pick - suitability[(int)i] < 0)
+                {
+                    if (newPopulation.Contains(population[(int)i]))
+                    {
+                        continue;
+                    }
+                    else
+                    {
+                        newPopulation.Add(population[(int)i]);
+                        break;
+                    }
+                }
+            }
+        }
+
+
+        //Choosing the parents with roullete selection
+        List<int[]> parents = new();
+        for (int l = 0; l < replacementPercent * population.Count; l++)
+        {
+            for (float i = 0, pick = random.NextSingle() * sum; (int)i < population.Count; i += 1)
+            {
+                if (pick - suitability[(int)i] < 0)
+                {
+                        parents.Add(population[(int)i]);
+                        break;
+                }
+                pick -= suitability[(int)i];
+            }
+        }
+
+        //Single point crossover loop
+        while (parents.Count>1)
+        {
+            //Choosing 2 random parents
+            int n = random.Next(parents.Count);
+            List<int> parent1 = parents[n].ToList();
+            parents.RemoveAt(n);
+
+            n = random.Next(parents.Count);
+            List<int> parent2 = parents[n].ToList();
+            parents.RemoveAt(n);
+
+            int[] child1 = new int[parent1.Count];
+            int[] child2 = new int[parent2.Count];
+            
+
+            //child1
+            List<int> temp = parent1.GetRange(0, parent1.Count/2);
+            List<int> temp1 = parent2.GetRange(0,parent2.Count);
+
+            for (int i = 0; i < parent1.Count / 2; i++)
+            {
+                child1[i] = temp[i];
+                temp1.Remove(temp[i]);
+            }
+
+            for (int i = parent1.Count/2; i < child1.Length; i++)
+            {
+                child1[i] = temp1.First();
+                temp1.RemoveAt(0);
+            }
+
+            
+
+
+            //child2
+            temp = parent2.GetRange(0, parent2.Count/2);
+            temp1 = parent1;
+
+            for (int i = 0; i < parent2.Count / 2; i++)
+            {
+                child2[i] = temp[i];
+                temp1.Remove(temp[i]);
+            }
+
+            for (int i = parent2.Count / 2; i < child2.Length; i++)
+            {
+                child2[i] = temp1.First();
+                temp1.RemoveAt(0);
+            }
+
+            newPopulation.Add(child1);
+            newPopulation.Add(child2);
+
+        }
+
+        //Mutation Loop
+        for (int i = 0; i < (int)mutationPercent*newPopulation.Count; i++)
+        {
+            int n = random.Next(newPopulation.Count);
+            int l = random.Next(newPopulation[n].Length);
+            int j = random.Next(newPopulation[n].Length);
+
+            int x = newPopulation[n][l];
+            newPopulation[n][l] = newPopulation[n][j];
+            newPopulation[n][j] = x;
+        }
+
+        population = newPopulation;
+
+        for (int i = 0; i < population.Count; i++)
+        {
+            suitability[i] = Suitability(population[i], adjacencyMatrix);
+        }
+
+        for (int i = population.Count; i < suitability.Length; i++)
+        {
+            suitability[i] = 0;
+        }
+
+
+        numberOfGenerations++;
+    }
+
+    float maxSuitability = suitability.Max();
+    
+    int[] solution = population[suitability.ToList().IndexOf(maxSuitability)];
+
+    return (maxSuitability, solution, numberOfGenerations);
+}
+
+
+
+static float Suitability(int[] chromosome, int[,] adjacencyMatrix)
+{
+    float sum = 0;
+    for (int i = 0; i < chromosome.Length-1; i++)
+    {
+        sum += adjacencyMatrix[chromosome[i],chromosome[i+1]];
+    }
+    sum += adjacencyMatrix[chromosome[chromosome.Length-1],chromosome[0]];
+
+    return 1 / sum;
+}
+
+
+static int[,] CreateAdjacencyMatrix(int numOfCities, int maxCost, Random random)
+{
+    //Initializing the symmetric adjacency matrix with costs up to 50
+    int[,] adjacencyMatrix = new int[numOfCities, numOfCities];
+
+    for (int i = 0; i < numOfCities; i++)
+    {
+        for (int j = i; j < numOfCities; j++)
+        {
+            if (i == j)
+            {
+                adjacencyMatrix[i, j] = 10000;
+            }
+            else
+            {
+                adjacencyMatrix[i, j] = random.Next(1, maxCost);
+            }
+        }
+        for (int j = 0; j < i; j++)
+        {
+            adjacencyMatrix[i, j] = adjacencyMatrix[j, i];
+        }
+    }
+
+    return adjacencyMatrix;
+}
+
+
+static List<int[]> InitializePopulation(int populationNumber, int numOfCities, Random random)
+{
+    List<int[]> population = new();
+
+    for (int i = 0; i < populationNumber; i++)
+    {
+        List<int> randomList = new();
+        int[] chromosome = new int[numOfCities];
+        for (int j = 0; j < chromosome.Length; j++)
+        {
+            while (true)
+            {
+                int n = random.Next(numOfCities);
+                if (!randomList.Contains(n))
+                {
+                    chromosome[j] = n;
+                    randomList.Add(n);
+                    break;
+                }
+            }
+        }
+        population.Add(chromosome);
+    }
+
+    return population;
+}
---
+++ slot machine/slot_machine/slot_machine/Form1.Designer.cs
@@ -0,0 +1,169 @@
+﻿namespace slot_machine
+{
+    partial class mainForm
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timerText1 = new System.Windows.Forms.Label();
+            this.spinButton = new System.Windows.Forms.Button();
+            this.bet = new System.Windows.Forms.TextBox();
+            this.scoreButton = new System.Windows.Forms.Button();
+            this.settingsButton = new System.Windows.Forms.Button();
+            this.timer = new System.Windows.Forms.Timer(this.components);
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // timerText1
+            // 
+            this.timerText1.AutoSize = true;
+            this.timerText1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(157)))), ((int)(((byte)(118)))), ((int)(((byte)(39)))));
+            this.timerText1.Font = new System.Drawing.Font("Segoe UI", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.timerText1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.timerText1.Location = new System.Drawing.Point(1078, 481);
+            this.timerText1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.timerText1.Name = "timerText1";
+            this.timerText1.Size = new System.Drawing.Size(32, 38);
+            this.timerText1.TabIndex = 12;
+            this.timerText1.Text = "3";
+            this.timerText1.Visible = false;
+            // 
+            // spinButton
+            // 
+            this.spinButton.BackgroundImage = global::slot_machine.Properties.Resources.spin1;
+            this.spinButton.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.spinButton.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.spinButton.FlatAppearance.BorderSize = 0;
+            this.spinButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.spinButton.Location = new System.Drawing.Point(926, 466);
+            this.spinButton.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.spinButton.Name = "spinButton";
+            this.spinButton.Size = new System.Drawing.Size(155, 54);
+            this.spinButton.TabIndex = 16;
+            this.spinButton.UseVisualStyleBackColor = true;
+            this.spinButton.Click += new System.EventHandler(this.SpinButton_Click);
+            // 
+            // bet
+            // 
+            this.bet.BackColor = System.Drawing.SystemColors.InfoText;
+            this.bet.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.bet.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.bet.Font = new System.Drawing.Font("Rockwell Condensed", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.bet.ForeColor = System.Drawing.SystemColors.Window;
+            this.bet.Location = new System.Drawing.Point(696, 481);
+            this.bet.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.bet.Name = "bet";
+            this.bet.Size = new System.Drawing.Size(208, 38);
+            this.bet.TabIndex = 23;
+            // 
+            // scoreButton
+            // 
+            this.scoreButton.BackgroundImage = global::slot_machine.Properties.Resources.score;
+            this.scoreButton.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.scoreButton.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.scoreButton.FlatAppearance.BorderSize = 0;
+            this.scoreButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.scoreButton.Location = new System.Drawing.Point(225, 480);
+            this.scoreButton.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.scoreButton.Name = "scoreButton";
+            this.scoreButton.Size = new System.Drawing.Size(166, 40);
+            this.scoreButton.TabIndex = 24;
+            this.scoreButton.UseVisualStyleBackColor = true;
+            this.scoreButton.Click += new System.EventHandler(this.scoreButton_Click);
+            // 
+            // settingsButton
+            // 
+            this.settingsButton.BackgroundImage = global::slot_machine.Properties.Resources.settings;
+            this.settingsButton.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.settingsButton.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.settingsButton.FlatAppearance.BorderSize = 0;
+            this.settingsButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.settingsButton.Location = new System.Drawing.Point(121, 459);
+            this.settingsButton.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.settingsButton.Name = "settingsButton";
+            this.settingsButton.Size = new System.Drawing.Size(79, 78);
+            this.settingsButton.TabIndex = 25;
+            this.settingsButton.UseVisualStyleBackColor = true;
+            this.settingsButton.Click += new System.EventHandler(this.settingsButton_Click);
+            // 
+            // timer
+            // 
+            this.timer.Interval = 900;
+            this.timer.Tick += new System.EventHandler(this.Timer_Tick);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.button1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button1.FlatAppearance.BorderSize = 0;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.button1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.button1.Location = new System.Drawing.Point(1099, 0);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(118, 36);
+            this.button1.TabIndex = 26;
+            this.button1.Text = "EXIT";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // mainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::slot_machine.Properties.Resources.slot31;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(1106, 616);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.settingsButton);
+            this.Controls.Add(this.scoreButton);
+            this.Controls.Add(this.bet);
+            this.Controls.Add(this.spinButton);
+            this.Controls.Add(this.timerText1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.MaximizeBox = false;
+            this.Name = "mainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Slot Machine";
+            this.Shown += new System.EventHandler(this.mainForm_Shown);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private Label timerText1;
+        private Button spinButton;
+        private TextBox bet;
+        private Button scoreButton;
+        private Button settingsButton;
+        private System.Windows.Forms.Timer timer;
+        private Button button1;
+    }
+}
